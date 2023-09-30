d8eaa832644a5f077a966a72ed5b90ec3519dd06
dengxiangbing <126864819+dengxiangbing@users.noreply.github.com>
2023-10-01 01:27:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            天翼云盘
 // @version         1.0.0
-// @description     签到、抽奖任务，每天保底获得100M+的空间
+// @description     签到、抽奖任务，每天保底获得100M+的空间 
 // @env             USERNAME、PASSWORD
 // ==/UserScript==
 
